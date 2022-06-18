74266c78d80ac3a1ca76d9ff0c969861d74c52a0
Koima-Sam <sam17koima@gmail.com>
2022-06-18 12:12:12 +0300

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  return word.split("").reverse().join("")===word ? true: false;
+  
 }
 
 /* 
   Add your pseudocode here
+  split and reverse original word
+
+  check if the reversed word is equal to original word
+    return true
+  if it's not equal
+  return false
 */
 
 /*
   Add written explanation of your solution here
+  
+  Does the word in reverse has the same meaning as the original word? if yes,
+  it should return true else false
+
+
+  
+
+
+
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log('Expecting: true');
+  console.log('=>',isPalindrome('abba') );
+
+  console.log('Expecting: true');
+  console.log('=>',isPalindrome('a') );
+
+  console.log('Expecting: false');
+  console.log('=>',isPalindrome('ab') );
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
@@ -20,6 +46,7 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
 }
 
 module.exports = isPalindrome;
